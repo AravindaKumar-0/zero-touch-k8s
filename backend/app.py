c3d40277a3111b4c4c9c26c1b48fc62308d01697
AravindaKumar-0 <aravindakumar.cse24le@cmrit.ac.in>
2026-08-17 21:39:57 +0530

    Initial Zero-Touch Kubernetes project

---
+++ backend/app.py
@@ -0,0 +1,42 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        if self.path == "/health":
+            response = {
+                "status": "healthy",
+                "service": "backend"
+            }
+
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(response).encode())
+
+        elif self.path == "/api":
+            response = {
+                "message": "Backend API is working",
+                "service": "zero-touch-k8s"
+            }
+
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(response).encode())
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+
+print("Backend running on port 8080")
+
+server.serve_forever()
